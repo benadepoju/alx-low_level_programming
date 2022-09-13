7ead93664e4b7873785fcaa1fd9eee81f6b95c5c
benadepoju <sunnybenpoju@gmail.com>
2022-09-13 06:11:20 -0700

    Another changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ _putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+{ putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0); }
