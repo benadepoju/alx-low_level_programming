d5936afb879b9590c5d606cb798216fa87d951f0
benadepoju <sunnybenpoju@gmail.com>
2022-09-28 11:51:31 -0700

    Updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h
 /**
-* _strlen_recursion - returns the length of a string
-* @s: string to calculate the length of
-*
-* Return: length of the string
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
 */
 int _strlen_recursion(char *s)
 {
